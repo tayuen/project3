06848de26f4653df042401c4b355839fdc79a9f5
tayuen <46732033+tayuen@users.noreply.github.com>
2019-04-24 12:31:32 -0500

    make update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function mapsF(){
     var map = new google.maps.Map(document.getElementById('map'),{
         center: {lat:6.213480, lng: 31.565460},
-        zoom: 10
+        zoom: 13
     });
    
 }
